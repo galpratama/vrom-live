9d4ecb7b2b04a3126bacf430b9167ee40f614172
Galih Pratama <hanamura.iost@gmail.com>
2023-03-16 14:58:12 +0700

    Membuat Seeder Akun Admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create new user with role admin
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'roles' => 'admin',
+        ]);
+    }
+}
